16f20f252c04e10fec461e476f25b88639f34d3a
karacsonyiz91 <karacsonyiz91@gmail.com>
2016-10-24 14:23:20 +0200

    okt 24

---
+++ todolist.py
@@ -0,0 +1,25 @@
+todolist=['sajt', 'disznó']
+n=input("Please specify a command [list,add,mark,archive]: ")
+
+if n=="list":
+    print("You saved the following to-do items: "+ "\n")
+    for i in range(0,len(todolist),1):
+        print(i+ "x"+ todolist[i])
+        #még nem jó init and str hiba
+elif n=="add":
+    add=input("Add an item: ")
+    todolist.append(add)
+    #new line és bevinni az új arrayt
+    print("Item added.")
+elif n=="mark":
+    print("You saved the following to-do items:")
+    #kiirja tartalmazott dolgokat
+    print("Which one you want to mark as completed: ")
+    #choose var kell és beállitani ha egyiket beűti mit csináljon
+    print("""varname""","is completed")
+elif n=="archive":
+    todolist=[]
+    print("All completed tasks got deleted.")
+else:
+    print("Chose again, please")
+#ez nem müködik
